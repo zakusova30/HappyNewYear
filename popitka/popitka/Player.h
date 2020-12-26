51f20df5d9e357266037198bfbbd856453982bc5
Никита Бутырев <bggniklol@gmail.com>
2020-12-26 20:47:42 +0300

    Описание класса player

--- popitka/popitka/Player.h
+++ popitka/popitka/Player.h
@@ -6,5 +6,33 @@ using namespace sf;
 class Player {
 private:
 	float x, y;
+public:
+	float w, h, dx, dy;
+	Player();
+	~Player();
+	Player(String File, float X, float Y, float W, float H);
+	int dir;
+	Image imagee;
+	Texture texturee;
+	Sprite sprite;
+	void update(float time);
+	void InteractionWithMap();
+	float speed;
+	Clock clock;
+	float time;
+	int key;
+	int hp;
+	void draw_p();
+	float CurrentFrame;
+	float coordinatePlayerX, coordinatePlayerY;
+	float GetPlayerCoordinateY();
+	void Damage();
+	void bDamage();
+	void Keys();
+	float GetPlayerCoordinateX();
+	RectangleShape hpbar;
+	RectangleShape keybar;
+	int win;
+
 
 };
