856db3ecfc5dfc7a4c6ff1a196c161657cd20ded
Zakusova Kate <zakusova30@gmail.com>
2020-12-26 20:00:20 +0300

    Создала Player.h - передаю в дальнейшую разработку

---
+++ popitka/popitka/Player.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "globals.h" // теперь всё работает
+#include <SFML/Graphics.hpp> /// подключаем заголовочный файл, который отвечает за работу с графикой
+using namespace sf;
+
+class Player {
+private:
+	float x, y;
+
+};
