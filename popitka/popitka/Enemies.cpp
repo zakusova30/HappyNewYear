9eb76f538504797ca46cf3b940060dc7d58c7a84
LizaStahievich <liza.12321@mail.ru>
2020-12-27 14:23:22 +0300

    Добавлен файл реализации Enemies.cpp

---
+++ popitka/popitka/Enemies.cpp
@@ -0,0 +1,26 @@
+#include "SFML/Graphics.hpp" 
+#include <iostream> 
+#include "Map.h" 
+#include "Globals.h" 
+#include "Enemies.h"
+#include "Player.h"  
+
+void Child_Enemies::checkCollisionWithMap()
+{
+
+}
+
+void Child_Enemies::drawing(float pX, float pY, Player & playerhp)
+{
+
+}
+
+Girl::Girl(int x, int y)
+{
+
+}
+
+Boy::Boy(int x, int y) 
+{
+
+}
