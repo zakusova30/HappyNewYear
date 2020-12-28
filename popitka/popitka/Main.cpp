6e349cd180175e12dc68c0dd573a403b5f20d61b
Zakusova Kate <zakusova30@gmail.com>
2020-12-28 11:29:29 +0300

    Создание скелета главной функции

---
+++ popitka/popitka/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <SFML/Graphics.hpp> /// подключаем заголовочный файл, который отвечает за работу с графикой
+#include "Map.h"
+#include "Globals.h"
+#include "Player.h"
+#include "Enemies.h"
+#include "Shooting.h"
+#include <list>
+
+using namespace std;
+using namespace sf;
+
+void winner(RenderWindow & window) // если игрок победит
+{
+
+}
+
+void lose(RenderWindow & window) // если игрок проишрает 
+{
+
+}
+
+int main() // главная функция
+{
+
+	return 0;
+}
+
